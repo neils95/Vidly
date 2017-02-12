f5bfda2ca8cb9d39bea324f70cfa7cdafc80a1b9
Neil Sanghrajka <neilsanghrajka@outlook.com>
2017-02-12 17:58:44 -0500

    Completed Ch2 Exercise.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private IEnumerable<Customer> customers = new List<Customer>
+        {
+            new Customer {Id=1, Name="Neil Sanghrajka" },
+            new Customer {Id=2, Name="Disha Sanghrajka"}
+
+        };
+
+        // GET: Customers must return a list of all customers
+        public ActionResult Index()
+        {
+            return View("Index", customers);
+        }
+
+        //Get: Customers/Details/{customerId}
+        [Route("Customers/details/{customerId:int:regex(1|2)}")]
+        public ActionResult Details(int customerId)
+        {
+            return View("Details", customers.SingleOrDefault(c => c.Id == customerId));
+        }
+
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -7,6 +7,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        //GET: Movies
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie {Id=1,Name="Shrek" },
+                new Movie {Id=2,Name="Wall-e" }
+            };
+
+
+            return View("Index", movies);
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
