788b3795f1ee63e5e7bd067e7d48d3dd89f1dbcb
Neil Sanghrajka <neilsanghrajka@outlook.com>
2017-02-13 16:26:13 -0500

    Section 3.26: Added MembershipType migration

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        //Navigation property, allows to naavigate from one type to another.
+        public MembershipType MembershipType { get; set; }
+
+        //Convention. Entity framework recognizes it as foreign key.
+        public byte MembershipTypeId { get; set; }
     }
 }
