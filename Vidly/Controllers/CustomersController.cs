9c38d764d7a90c21e5e6a32fc28513e1cd3b4f60
Neil Sanghrajka <neilsanghrajka@outlook.com>
2017-02-13 19:37:50 -0500

    Section 3.30: Eager loading of MembershipType with customer.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,35 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
-    public class CustomersController : Controller
-    {
-        private ApplicationDbContext _context;
+	public class CustomersController : Controller
+	{
+		private ApplicationDbContext _context;
 
-        public CustomersController()
-        {
-            //Initialize DbContext in constructor
-            _context = new ApplicationDbContext();
-        }
+		public CustomersController()
+		{
+			//Initialize DbContext in constructor
+			_context = new ApplicationDbContext();
+		}
 
-        // GET: Customers must return a list of all customers
-        public ActionResult Index()
-        {
-            return View("Index", _context.Customers.ToList());
-        }
+		// GET: Customers must return a list of all customers
+		public ActionResult Index()
+		{
+			//Eager loading using Inlclude()
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+			return View("Index", customers);
+		}
 
-        //Get: Customers/Details/{customerId}
-        [Route("Customers/details/{customerId:int:regex(1|2)}")]
-        public ActionResult Details(int customerId)
-        {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == customerId);
-            if (customer == null)
-                return HttpNotFound();
+		//Get: Customers/Details/{customerId}
+		[Route("Customers/details/{customerId:int:regex(1|2)}")]
+		public ActionResult Details(int customerId)
+		{
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == customerId);
+			if (customer == null)
+				return HttpNotFound();
 
-            return View("Details", customer);
-        }
+			return View("Details", customer);
+		}
 
-    }
+	}
 }
