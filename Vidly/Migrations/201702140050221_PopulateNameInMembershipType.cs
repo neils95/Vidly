455ddd47bc1520d626e6dbb500278f57b5bd8ad0
Neil Sanghrajka <neilsanghrajka@outlook.com>
2017-02-13 20:32:41 -0500

    Section 3 Excersize 1: Added MembershipType name column, updated Customers/Details/id view

---
+++ Vidly/Migrations/201702140050221_PopulateNameInMembershipType.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+	using System;
+	using System.Data.Entity.Migrations;
+	
+	public partial class PopulateNameInMembershipType : DbMigration
+	{
+		public override void Up()
+		{
+			Sql("UPDATE MembershipTypes SET name ='Pay as You Go' WHERE Id=1");
+			Sql("UPDATE MembershipTypes SET name ='Monthly' WHERE Id=2");
+			Sql("UPDATE MembershipTypes SET name ='Quarterly' WHERE Id=3");
+			Sql("UPDATE MembershipTypes SET name ='Annual' WHERE Id=4");
+
+		}
+
+		public override void Down()
+		{
+		}
+	}
+}
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -5,11 +5,12 @@ using System.Web;
 
 namespace Vidly.Models
 {
-    public class MembershipType
-    {
-        public byte Id { get; set; }
-        public short SignUpFee { get; set; }
-        public byte DurationInMonths { get; set; }
-        public byte DiscountRate { get; set; }
-    }
+	public class MembershipType
+	{
+		public byte Id { get; set; }
+		public string name { get; set; }
+		public short SignUpFee { get; set; }
+		public byte DurationInMonths { get; set; }
+		public byte DiscountRate { get; set; }
+	}
 }
